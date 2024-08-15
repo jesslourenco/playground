922203fbfc54a7ad11c57107d10b4b73ebced342
jess <70455379+jesslourenco@users.noreply.github.com>
2024-08-15 16:24:04 -0400

    update view response

--- lets-go/snippetbox/main.go
+++ lets-go/snippetbox/main.go
@@ -18,8 +18,7 @@ func snippetView(w http.ResponseWriter, r *http.Request) {
 		http.NotFound(w, r)
 		return
 	}
-	msg := fmt.Sprintf("Display a specific snippet with ID %d...", id)
-	w.Write([]byte(msg))
+	fmt.Fprintf(w, "Display a specifc snippet with D %d...", id)
 }
 
 // Add a snippetCreate handler function.
